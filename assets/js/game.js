d085f571fdfcb4a73d0cfcbbb2bb035555cbbb68
Tyler <tylersundquist3@gmail.com>
2022-02-01 18:49:16 -0700

    set up questions variable

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,22 @@
-var questionCounter = 0;
-
-var beginQuiz = function() {
-    
-}
+var questions = [
+  {
+    question: "What is the rarest M&M flavor?",
+    results: ["Green", "Red", "Brown", "Blue"],
+    answer: "Brown",
+  },
+  {
+    question: "In what year were the first Air Jordan sneakers released?",
+    results: ["1984", "1985", "1986", "1987"],
+    answer: "1984",
+  },
+  {
+    question: "In which European city would you find Orly airport?",
+    results: ["Vienna", "Rome", "Paris", "Amsterdam"],
+    answer: "Paris",
+  },
+  {
+    question: "Which singers real name is Stefani Joanne Angelina Germanotta?",
+    results: ["Katy Perry", "Lady Gaga", "Adele", "Madonna"],
+    answer: "Lady Gaga",
+  },
+];
